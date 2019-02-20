cbabd2d859f2b8910e126a4d98eba304b1c20fd5
He Yangxuan <yangxuan8282@gmail.com>
2019-02-20 15:23:12 +0000

    kernel: rpi add firmware

--- kernel/rpi/scripts/install_kernel_debian.sh
+++ kernel/rpi/scripts/install_kernel_debian.sh
@@ -2,6 +2,18 @@
 
 set -e
 
+get_rpi_firmware() {
+	mkdir -p /lib/firmware/brcm
+
+	for wifi_fw in 43430-sdio.bin 43430-sdio.txt 43455-sdio.bin 43455-sdio.clm_blob 43455-sdio.txt ; do
+		wget -P /lib/firmware/brcm https://github.com/RPi-Distro/firmware-nonfree/raw/master/brcm/brcmfmac${wifi_fw} 
+	done
+
+	#for bt_fw in BCM43430A1.hcd BCM4345C0.hcd ; do
+	#	wget -P /lib/firmware/brcm https://github.com/RPi-Distro/bluez-firmware/raw/master/broadcom/${bt_fw}
+	#done
+}
+
 install_kernel_armhf() {
 	if [ ! -f /etc/apt/sources.list.d/raspi.list ]; then
 		apt-get update && apt-get install -y dirmngr
@@ -20,6 +32,8 @@ install_kernel_arm64() {
 	wget https://github.com/sakaki-/bcmrpi3-kernel/releases/download/$KERVER/bcmrpi3-kernel-$KERVER.tar.xz -O- | tar -C / -xJf -
 	rm -rf /var/lib/apt/lists/* /tmp/*
 }
+
+get_rpi_firmware
 
 case "$ARCH" in
 	armhf ) install_kernel_armhf;;
