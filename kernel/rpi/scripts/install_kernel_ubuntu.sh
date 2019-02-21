342db70d49db86e4fc551a7b63b994f6f50f2d91
He Yangxuan <yangxuan8282@gmail.com>
2019-02-21 05:07:48 +0000

    rootfs: add ubuntu for rpi

---
+++ kernel/rpi/scripts/install_kernel_ubuntu.sh
@@ -0,0 +1,1 @@
+install_kernel_debian.sh
--- rootfs/ubuntu/scripts/install_base.sh
+++ rootfs/ubuntu/scripts/install_base.sh
@@ -3,5 +3,7 @@
 set -e
 
 apt-get install -y network-manager
+# dirty fix for wired interface
+mv /usr/lib/NetworkManager/conf.d/10-globally-managed-devices.conf /usr/lib/NetworkManager/conf.d/10-globally-managed-devices.conf.orig
 
 rm -rf /var/lib/apt/lists/* /tmp/*
