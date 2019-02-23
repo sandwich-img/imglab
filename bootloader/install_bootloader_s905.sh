3b344750403633094f78d2fc3748666eec1d0d2f
He Yangxuan <yangxuan8282@gmail.com>
2019-02-23 02:57:01 +0000

    bootloader: add s905 debian bootloader

--- bootloader/install_bootloader_s905.sh
+++ bootloader/install_bootloader_s905.sh
@@ -6,6 +6,8 @@ cd "$(dirname "$0")"
 
 DEST=../target/mnt
 
+rm -f $DEST/boot/*autoscript* $DEST/boot/uInitrd || true
+
 cp files/$DEVICE/$DISTRO/* $DEST/boot/
 
 cd $DEST/boot
@@ -13,6 +15,7 @@ cd $DEST/boot
 case $DISTRO in
 	alpine) INITRD=initramfs-amlogic;;
 	archlinux) INITRD=initramfs-linux.img;;
+	debian | ubuntu) INITRD=initrd.img-4.18.7-aml-s9xxx;;
 esac
 
 mkimage -C none -A arm -T script -d aml_autoscript.cmd aml_autoscript
@@ -20,4 +23,4 @@ mkimage -C none -A arm -T script -d s905_autoscript.cmd s905_autoscript
 mkimage -C none -A arm -T script -d emmc_autoscript.cmd emmc_autoscript
 mkimage -A arm64 -O linux -T ramdisk -C gzip -n uInitrd -d $INITRD uInitrd
 
-sleep 6 
+sleep 6
