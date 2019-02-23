d0096657949aaac4e62c7d32c2d3b7906b9d191a
He Yangxuan <yangxuan8282@gmail.com>
2019-02-23 02:56:17 +0000

    kernel: fix s905 debian kernel

--- kernel/s905/scripts/install_kernel_debian.sh
+++ kernel/s905/scripts/install_kernel_debian.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 
-set -xe
+set -e
 
-wget https://github.com/yangxuan8282/phicomm-n1/releases/download/150balbes_kernel/kernel_4.18.7_20180922.tar.gz
-tar xf *.tar.gz
-cp -R --no-preserve=mode,ownership kernel_*/boot/* /boot/
-cp -a kernel_*/lib/* /lib/
-sed -i "s|root=LABEL=ROOTFS|root=UUID=${ROOT_UUID}|" /boot/uEnv.ini
+wget https://github.com/yangxuan8282/img_host/raw/master/files/kernel/s905/kernel_4.18.7.tar.gz
+tar xf *.tar.gz --strip=1 -C /
+sed -i "s|ROOTFS|rootfs|" /boot/uEnv.ini
 rm -rf kernel_*
-
-wget https://github.com/yangxuan8282/phicomm-n1/releases/download/dtb/meson-gxl-s905d-phicomm-n1.dtb
-mv *.dtb /boot/dtb.img
