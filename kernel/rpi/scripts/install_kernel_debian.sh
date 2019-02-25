069d5ebed5f2e5aa1393318e6a2801fb6e292e48
He Yangxuan <yangxuan8282@gmail.com>
2019-02-25 10:46:54 +0000

    kernel: rpi update arm64 kernel to 4.19.23.20190223

--- kernel/rpi/scripts/install_kernel_debian.sh
+++ kernel/rpi/scripts/install_kernel_debian.sh
@@ -17,7 +17,7 @@ get_rpi_firmware() {
 install_kernel_armhf() {
 	if [ ! -f /etc/apt/sources.list.d/raspi.list ]; then
 		apt-get update && apt-get install -y dirmngr
-		echo "deb http://archive.raspberrypi.org/debian/ stretch main ui" | tee /etc/apt/sources.list.d/raspi.list
+		echo "deb http://archive.raspberrypi.org/debian/ "$DEBIAN_BRANCH" main ui" | tee /etc/apt/sources.list.d/raspi.list
 		wget -qO - http://archive.raspberrypi.org/debian/raspberrypi.gpg.key | apt-key add -
 		apt-get update && apt-get upgrade -y
 	fi
@@ -28,7 +28,7 @@ install_kernel_armhf() {
 
 install_kernel_arm64() {
 	apt-get update && apt-get install -y ca-certificates xz-utils
-	KERVER=4.14.93.20190115
+	KERVER=4.19.23.20190223
 	wget https://github.com/sakaki-/bcmrpi3-kernel/releases/download/$KERVER/bcmrpi3-kernel-$KERVER.tar.xz -qO- | tar -C / -xJf -
 	rm -rf /var/lib/apt/lists/* /tmp/*
 }
