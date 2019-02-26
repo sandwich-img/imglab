904760095a4a10749aa6945b85053ba208eb8e39
He Yangxuan <yangxuan8282@gmail.com>
2019-02-26 05:34:00 +0000

    rootfs: debian add weston

--- rootfs/debian/scripts/install_weston.sh
+++ rootfs/debian/scripts/install_weston.sh
@@ -4,4 +4,8 @@ set -e
 
 apt-get install -y weston xwayland
 
+apt-get install -y network-manager
+
+su $USER sh -c "mkdir -p /home/${USER}/.config && cp /configs/dotfiles/weston.ini /home/${USER}/.config/weston.ini"
+
 rm -rf /var/lib/apt/lists/* /tmp/*
