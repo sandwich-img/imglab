463dc461f1f0125ed938fcc4860ddc16469d7478
He Yangxuan <yangxuan8282@gmail.com>
2019-03-05 03:13:52 +0000

    rootfs: improve alpine mate desktop

--- rootfs/alpine/scripts/install_mate.sh
+++ rootfs/alpine/scripts/install_mate.sh
@@ -6,4 +6,7 @@ apk add --no-cache xorg-server xf86-video-fbdev xf86-input-libinput
 
 apk add --no-cache mate-desktop-environment dbus-x11 lxdm
 
+sed -i "s/^# autologin=dgod/autologin=$USER/" /etc/lxdm/lxdm.conf
+sed -i 's|^# session=/usr/bin/startlxde|session=/usr/bin/mate-session|' /etc/lxdm/lxdm.conf
 
+rc-update add lxdm default
